a000acc579f7fbb712490e1c3de9e010f719d119
Kush7781 <73100821+Kush7781@users.noreply.github.com>
2024-06-14 22:05:15 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import SignInPage from "./Pages/SignInPage";
+import SignUpPage from "./Pages/SignUpPage";
+import { AuthContext } from "./Context/AuthContext";
+import "./App.css";
+import "./Styles/Navbar.css";
+import AddProperty from "./Pages/AddProperty";
+import SellerProperty from "./Pages/SellerProperty";
+import Properties from "./Pages/Properties";
 
 function App() {
+  const { isLoggedIn, user } = useContext(AuthContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn && user?.userType === "seller" ? (
+                <header className="App-header">
+                  <h1>Sell Your Perfect Space with Rentify</h1>
+                  <Link to="/SellerProperty">
+                    <button>Get Started</button>
+                  </Link>
+                </header>
+              ) : (
+                <header className="App-header">
+                  <h1>Find Your Perfect Space with Rentify</h1>
+                  <Link to="/Properties">
+                    <button>Get Started</button>
+                  </Link>
+                </header>
+              )
+            }
+          />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/AddProperty" element={<AddProperty />} />
+          <Route path="/SellerProperty" element={<SellerProperty />} />
+          <Route path="/Properties" element={<Properties />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
